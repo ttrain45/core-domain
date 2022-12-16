7d3499401c3c2d486b85b73f9cc5946fceee11de
EC2 Default User <ec2-user@ip-172-31-80-172.ec2.internal>
2022-12-16 18:39:36 +0000

    Reduction in dependencies and rewrite of code to test in real-time with lambda console

--- player/lambda_function/runtime/stream_add_player.py
+++ player/lambda_function/runtime/stream_add_player.py
@@ -7,18 +7,16 @@ def handler(event, context):
     for payload in event:
         print("Decoded payload: " + str(base64.b64decode(payload["data"])))
 
-        add_player_entries = {
-                "source": "ingest-api",
-                "detail-type": "player",
-                "detail": json.loads(base64.b64decode(payload["data"]).decode("utf-8"))
-            }
-
-        print(json.dumps(add_player_entries))
-
-        return {
+        result = {
             "statusCode": 200,
+            "source": "ingest-api",
+            "detail-type": "player",
             "headers": {
                 "Content-Type": "application/json"
             },
-            "body": add_player_entries
+            "detail": json.loads(base64.b64decode(payload["data"]).decode("utf-8")),
         }
+
+        print(result)
+
+        return result
